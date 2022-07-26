17246f98acc4c363a556a16972e53aa17dbca4fd
Jackson Zhipeng Chang <zchang0302@gmail.com>
2022-07-25 20:12:15 -0600

    skip the icon that has no url

--- jackson-website/src/components/elements/Hero.js
+++ jackson-website/src/components/elements/Hero.js
@@ -21,20 +21,22 @@ function Hero() {
         <div className="columns is-mobile">
           <div className="column"></div>
           {Resume.basics.profiles.map((value, index) => {
-            return (
-              <div key={index} className="column has-text-centered">
-                <a
-                  href={value.url}
-                  target="blank"
-                  className="is-hovered"
-                  title={value.network}
-                >
-                  <span className="icon is-medium is-">
-                    <i className={value.x_icon}></i>
-                  </span>
-                </a>
-              </div>
-            );
+            if (value.url){
+              return (
+                <div key={index} className="column has-text-centered">
+                  <a
+                    href={value.url}
+                    target="blank"
+                    className="is-hovered"
+                    title={value.network}
+                  >
+                    <span className="icon is-medium is-">
+                      <i className={value.x_icon}></i>
+                    </span>
+                  </a>
+                </div>
+              );
+            }
           })}
           <div className="column"></div>
         </div>
